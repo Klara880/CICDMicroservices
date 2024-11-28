8e72b56c33443208010d3d16210d074960bcd816
clara <cmtob@hotmail.com>
2024-11-28 09:34:44 -0500

    upload microservices

---
+++ MicroservieItem/src/test/java/com/bancolombia/app/MicroservieItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancolombia.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservieItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
